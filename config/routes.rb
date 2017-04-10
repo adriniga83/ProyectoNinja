1256611544032c65465d4bfa26eba718d6e1c755
Adrian N <adriniga@gmail.com>
2017-04-10 10:06:25 +0200

    ANTES DE JUEGOS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :estanteria
-  devise_for :users
+  #devise_for :users
+  devise_for :users, :controllers => { :registrations => 'users/registrations' }
   get 'inicio/index'
   root :to => "inicio#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
